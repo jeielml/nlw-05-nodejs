558f59143be81d2a47b5a91b000f185657598613
BETHA\jeiel.lopes <jeiel.lopes@betha.com.br>
2021-04-22 08:06:28 -0300

    incluindo objeto usuario no payload de retorno da lista de mensagens

--- src/services/MessagesServices.ts
+++ src/services/MessagesServices.ts
@@ -32,11 +32,11 @@ class MessagesServices {
     async listByUser(user_id: string) {
         const messagesRepository = getCustomRepository(MessagesRepository)
 
-        const list = await messagesRepository.find({user_id,})
-        // const list = await messagesRepository.find({
-        //     where: {user_id},
-        //     relations: ["user"]
-        // })
+        // const list = await messagesRepository.find({user_id,})
+        const list = await messagesRepository.find({
+            where: {user_id},
+            relations: ["user"]
+        })
 
         return list
     }
