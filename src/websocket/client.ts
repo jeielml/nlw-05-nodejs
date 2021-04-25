287cb32e9a66027602909c3d4b5c89723a6b1330
BETHA\jeiel.lopes <jeiel.lopes@betha.com.br>
2021-04-25 18:38:53 -0300

    Salvando a mensagem

--- src/websocket/client.ts
+++ src/websocket/client.ts
@@ -1,30 +1,38 @@
 import { http, io } from "../http";
 import { ConnectionsServices } from "../services/ConnectionsServices";
+import { MessagesServices } from "../services/MessagesServices";
 import { UsersServices } from "../services/UsersServices";
 
+interface IParams {
+    text: string,
+    email: string
+}
 io.on("connect", (socket) => {
     const connectionsService =  new ConnectionsServices()
     const usersServices =  new UsersServices()
+    const messagesServices = new MessagesServices()
 
     socket.on("client_first_access", async (params) => {
         const socket_id = socket.id
-        const {text , email} = params
+        const {text , email} = params as IParams
+        let user_id = null 
 
         const userExists = await usersServices.findByEmail(email)
 
         if(!userExists) {
             const user = await usersServices.create(email)
-
+            user_id = user.id
             await connectionsService.create({
                 socket_id, 
-                user_id: user.id
+                user_id
             })
         } else {
+            user_id = userExists.id
             const connection = await connectionsService.findByUserId(userExists.id)
             if(!connection){
                 await connectionsService.create({
                     socket_id, 
-                    user_id: userExists.id
+                    user_id
                 })
             } else {
                 connection.socket_id = socket_id
@@ -32,6 +40,11 @@ io.on("connect", (socket) => {
             }
             console.log(connection)
         }
+
+        await messagesServices.create({
+            text, 
+            user_id
+        })
 
     })
 })
